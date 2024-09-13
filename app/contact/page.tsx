d0300e45524ecfeae693c59aa49fd27fbc5574fa
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-09-13 23:33:08 +1000

    disabled contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,22 @@
-'use client';
-import ContactForm from '@/components/ContactForm';
+// 'use client';
+// import ContactForm from '@/components/ContactForm';
 
-const ContactPage = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Contact Me
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Fill out the form below, and I&apos;ll get back to you shortly.
-          </p>
-        </div>
-        <ContactForm />
-      </div>
-    </div>
-  );
-};
+// const ContactPage = () => {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
+//       <div className="max-w-md w-full space-y-8">
+//         <div>
+//           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+//             Contact Me
+//           </h2>
+//           <p className="mt-2 text-center text-sm text-gray-600">
+//             Fill out the form below, and I&apos;ll get back to you shortly.
+//           </p>
+//         </div>
+//         <ContactForm />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ContactPage;
+// export default ContactPage;

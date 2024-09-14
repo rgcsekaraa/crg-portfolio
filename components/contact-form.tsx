946dc47927bf85841b1a11880e01357099d0d8e0
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-09-14 15:12:56 +1000

    removed unused component

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import { ChevronLeft, ChevronRight, Send } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"

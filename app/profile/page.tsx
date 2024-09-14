828962a78f688b6a27c2ecac1e64cc271bbb42a1
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-09-14 23:54:37 +1000

    tick icon fixed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -249,16 +249,20 @@ const HonorsAndAwards = () => (
 
 const TickIcon = () => (
   <svg
-    className='w-6 h-6 text-green-600'
-    fill='none'
-    stroke='currentColor'
-    strokeWidth='2'
-    viewBox='0 0 24 24'
-    xmlns='http://www.w3.org/2000/svg'
+    width="24"  // Fixed width
+    height="24" // Fixed height
+    className="flex-shrink-0 text-green-600" // Prevent shrinking in flex containers
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <path strokeLinecap='round' strokeLinejoin='round' d='M9 12l2 2l4-4' />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2l4-4" />
   </svg>
 );
+
+
 
 const Status = () => (
   <div className='space-y-4'>
